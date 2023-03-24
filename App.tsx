7bc04cd4eacd9172420830075b791f4821cfafd2
GJ Tiquia <gershom.tiquia@9cat.io>
2023-03-24 10:23:32 +0800

    Moving between screens

--- App.tsx
+++ App.tsx
@@ -1,23 +1,66 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-export default function App() {
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined
+};
+
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+function HomeScreen({ route, navigation }: HomeScreenProps) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+
+function DetailsScreen({ navigation }: DetailsScreenProps) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+
+        // navigation.navigate('Details') will do nothing as we are already in the Details screen
+        // using navigation.push('Details') will allow us to add a new route to the navigation stack
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Overview' }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # React Navigation Fundamentals
 
 A small project following the [React Navigation Fundamentals](https://reactnavigation.org/docs/getting-started) tutorial for learning [React Navigation](https://reactnavigation.org/).
+
+Useful Links:
+- [React Navigation: Type checking with TypeScript](https://reactnavigation.org/docs/typescript/)
