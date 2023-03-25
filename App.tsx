466bed1cac708eb448e0f07b3af38d6efffe88d0
GJ Tiquia <gershom.tiquia@gmail.com>
2023-03-25 19:26:12 +0800

    installed React Native Paper and merged themes

--- App.tsx
+++ App.tsx
@@ -1,8 +1,34 @@
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps
+} from '@react-navigation/native-stack';
+
+import {
+  adaptNavigationTheme,
+  Provider as PaperProvider,
+  MD3DarkTheme,
+  MD3LightTheme,
+} from 'react-native-paper';
+
+import merge from 'deepmerge';
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = merge(MD3DarkTheme, LightTheme);
+const CombinedDarkTheme = merge(MD3LightTheme, DarkTheme);
 
 type RootStackParamList = {
   Home: undefined; // undefined => optional route.params
@@ -61,28 +87,32 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: 'Overview',
-            headerRight: () => (
-              <Button
-                onPress={() => alert('This is a button!')}
-                title="Info"
-              />
-            )
-          }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          initialParams={{ itemId: 42 }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider theme={CombinedDarkTheme}>
+      <NavigationContainer theme={CombinedDarkTheme}>
+        <StatusBar style='auto'></StatusBar>
+
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              title: 'Overview',
+              headerRight: () => (
+                <Button
+                  onPress={() => alert('This is a button!')}
+                  title="Info"
+                />
+              )
+            }}
+          />
+          <Stack.Screen
+            name="Details"
+            component={DetailsScreen}
+            initialParams={{ itemId: 42 }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # React Navigation Fundamentals
 
-A small project following the [React Navigation Fundamentals](https://reactnavigation.org/docs/getting-started) tutorial for learning [React Navigation](https://reactnavigation.org/).
+A small [React Native project](https://reactnative.dev/) bootstrapped with [Expo](https://expo.dev/) following the [React Navigation Fundamentals](https://reactnavigation.org/docs/getting-started) tutorial for learning [React Navigation](https://reactnavigation.org/), and following the [React Native Paper](https://reactnativepaper.com/) documentation to merge and integrate the two packages.
 
-Useful Links:
+Links for Reference:
 - [React Navigation: Type checking with TypeScript](https://reactnavigation.org/docs/typescript/)
+- [Combining Themes with React Native Paper](https://callstack.github.io/react-native-paper/docs/guides/theming-with-react-navigation#combining-theme-objects)
+- [Integrate React Native Paper AppBar with React Navigation](https://callstack.github.io/react-native-paper/docs/guides/react-navigation)
